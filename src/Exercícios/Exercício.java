80497930178f83082b5399f53bcc9c3aec6d9fd6
unknown <labreu690@gmail.com>
2020-05-11 20:13:50 -0300

    Exercício 1

--- src/Exercícios/Exercício.java
+++ src/Exercícios/Exercício.java
@@ -1,5 +1,44 @@
 package Exercícios;
 
-public class Exercício {
+import java.util.Scanner;
 
+public class Exercício {
+	
+	public static void main(String[] agrs) {
+		String x;
+
+		Scanner leitor = new Scanner (System.in);
+		
+		System.out.println("Digite o Código do Produto: ");
+		
+		x = leitor.next();
+		
+		switch(x) {
+		
+		case "001":
+			System.out.println("Parafuso");
+			break;
+			
+		case "002":
+			System.out.println("Porca");
+			break;
+			
+		case "003":
+			System.out.println("Prego");
+			break;
+			
+		case "004":
+			System.out.println("Ferramenta");
+			break;
+			
+		default:
+			System.out.println("Diversos");
+		}
+	}
+	
 }
+
+	
+	
+	
+
