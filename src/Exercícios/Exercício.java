100c031ad7b96aaf17f24136009d4f05fcb48c83
unknown <labreu690@gmail.com>
2020-05-11 21:33:33 -0300

    Exercício 4

--- src/Exercícios/Exercício.java
+++ src/Exercícios/Exercício.java
@@ -1,5 +1,44 @@
 package Exercícios;
 
-public class Exercício {
+import java.util.Scanner;
 
+public class Exercício {
+	
+	public static void main(String[] args) {
+	float x, y, z;
+	int escolhaDeCalculo;
+	
+	Scanner leitor = new Scanner (System.in);
+	
+	System.out.println("Digite a primeira nota: ");
+	x = leitor.nextFloat();
+	
+	System.out.println("Digite a segunda nota: ");
+	y = leitor.nextFloat();
+			
+	System.out.println("Digite a terceira nota: ");
+	z = leitor.nextFloat();
+	
+	System.out.println("Por qual método você deseja calcular as notas? ");
+	
+	System.out.println("Digite 1 para Média Aritmética");
+	
+	System.out.println("Digite 2 para Média Ponderada");
+	
+	escolhaDeCalculo = leitor.nextInt();
+	
+	switch(escolhaDeCalculo){
+		case 1:
+			System.out.println((x + y + z)/3);
+		break;
+		
+		case 2:
+			System.out.println(((x * 3) + (y *3) + (z *4)) /10);
+		break;
+		
+	default:
+		System.out.println("Opção Inválida");
+		break;
+	}
+	}
 }
